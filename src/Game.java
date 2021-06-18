5329ffcb6d1fa0226304548b7b741292d8512f93
andreysyrbu <andreysyrbumacmini@gmail.com>
2021-06-18 09:39:33 +0300

    modified HangmanStages.java and Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,16 @@
 public class Game {
+
+    public static void createNumber() {}
+
+    public static void takeGuess() {
+    }
+
+    public static void checkGuess() {
+    }
+
+    public static void run() {
+    }
+
+    public static void main(String[] args) {
+    }
 }
--- src/HangmanStages.java
+++ src/HangmanStages.java
@@ -1,2 +1,124 @@
 public class HangmanStages {
+
+    public static final char a = 92;
+
+    public static void stage1() {
+        System.out.println("XXXXXXX        ");
+        System.out.println("X     X        ");
+        System.out.println("X    ___       ");
+        System.out.println("X   (. .)      ");
+        System.out.println("X    ___       ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+    }
+
+    public static void stage2() {
+        System.out.println("XXXXXXX        ");
+        System.out.println("X     X        ");
+        System.out.println("X    ___       ");
+        System.out.println("X   (. .)      ");
+        System.out.println("X    ___       ");
+        System.out.println("X     X        ");
+        System.out.println("X     X        ");
+        System.out.println("X     X        ");
+        System.out.println("X     X        ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+    }
+
+    public static void stage3() {
+        System.out.println("XXXXXXX        ");
+        System.out.println("X     X        ");
+        System.out.println("X    ___       ");
+        System.out.println("X "+a+" (. .)  ");
+        System.out.println("X  "+a+" ___   ");
+        System.out.println("X   "+a+" X    ");
+        System.out.println("X    "+a+"X    ");
+        System.out.println("X     X        ");
+        System.out.println("X     X        ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+    }
+
+    public static void stage4() {
+        System.out.println("XXXXXXX        ");
+        System.out.println("X     X        ");
+        System.out.println("X    ___       ");
+        System.out.println("X "+a+" (. .) /");
+        System.out.println("X  "+a+" ___ / ");
+        System.out.println("X   "+a+" X /  ");
+        System.out.println("X    "+a+"X/   ");
+        System.out.println("X     X        ");
+        System.out.println("X     X        ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+        System.out.println("X              ");
+    }
+
+    public static void stage5() {
+        System.out.println("XXXXXXX        ");
+        System.out.println("X     X        ");
+        System.out.println("X    ___       ");
+        System.out.println("X "+a+" (. .) /");
+        System.out.println("X  "+a+" ___ / ");
+        System.out.println("X   "+a+" X /  ");
+        System.out.println("X    "+a+"X/   ");
+        System.out.println("X     X        ");
+        System.out.println("X     X        ");
+        System.out.println("X    /         ");
+        System.out.println("X   /          ");
+        System.out.println("X              ");
+    }
+
+    public static void stage6() {
+        System.out.println("XXXXXXX        ");
+        System.out.println("X     X        ");
+        System.out.println("X    ___       ");
+        System.out.println("X "+a+" (. .) /");
+        System.out.println("X  "+a+" ___ / ");
+        System.out.println("X   "+a+" X /  ");
+        System.out.println("X    "+a+"X/   ");
+        System.out.println("X     X        ");
+        System.out.println("X     X        ");
+        System.out.println("X    / "+a+"   ");
+        System.out.println("X   /   "+a+"  ");
+        System.out.println("X              ");
+    }
+
+    public static void stage7() {
+        System.out.println("XXXXXXX        ");
+        System.out.println("X     X        ");
+        System.out.println("X    ___  "+a+"");
+        System.out.println("X "+a+" (. .) /");
+        System.out.println("X  "+a+" ___ / ");
+        System.out.println("X   "+a+" X /  ");
+        System.out.println("X    "+a+"X/   ");
+        System.out.println("X     X        ");
+        System.out.println("X     X        ");
+        System.out.println("X    / "+a+"   ");
+        System.out.println("X   /   "+a+"  ");
+        System.out.println("X              ");
+    }
+
+    public static void stage8() {
+        System.out.println("XXXXXXX        ");
+        System.out.println("X     X        ");
+        System.out.println("X /  ___  "+a);
+        System.out.println("X "+a+" (. .) /");
+        System.out.println("X  "+a+" ___ / ");
+        System.out.println("X   "+a+" X /  ");
+        System.out.println("X    "+a+"X/   ");
+        System.out.println("X     X        ");
+        System.out.println("X     X        ");
+        System.out.println("X    / "+a+"   ");
+        System.out.println("X   /   "+a+"  ");
+        System.out.println("X              ");
+    }
 }
